174ee193bc87d78483e727e00c97b4f24aae78b8
Unknown <jangarretsen01@gmail.com>
2020-02-18 14:48:05 +0100

    daily commit

--- stemwijzer.js
+++ stemwijzer.js
@@ -1,51 +1,100 @@
-function onload(){
-    createElement("button", {onclick: start, innerText: "Start"}, function(element){
+function onload() {
+    createElement("button", {onclick: start, innerText: "Start"}, function (element) {
         document.getElementById("container").appendChild(element);
     });
 
 }
 
-function start(){
+function start() {
     document.getElementById("container").firstChild.remove();
     question();
 }
 
-function question(index = 0, data = {eens: 0, oneens: 0, geen: 0}){
+function question(index = 0, answers = []) {
     let container = document.getElementById("container");
-    while(container.firstChild){
+    data = {Eens: "pro", Oneens: "contra", Geen: "none"};
+    questions = subjects;
+    checkboxes = [];
+    questionInfo = subjects[index];
+    while (container.firstChild) {
         container.firstChild.remove();
     }
 
-    questionInfo = subjects[index];
-    party = questionInfo.parties;
+    if (subjects.length <= index){
+        let ul = createElement("ul", {}, function (element){
+            container.appendChild(element);
+        });
+        questions.forEach(value =>{
+            let li = createElement("li");
+            let checkbox = createElement("input", {type: "checkbox"});
+            let label = createElement("label", {innerText: value.title});
 
-    createElement("h1", {innerText: questionInfo.title}, function(element){
-        container.appendChild(element);
-    });
-    createElement("h2", {innerText: questionInfo.statement}, function(element){
-        container.appendChild(element);
-    });
+            checkboxes.push(checkbox);
+            ul.appendChild(li);
+            li.appendChild(checkbox);
+            li.appendChild(label);
+        });
+        createElement("button", {innerText: "Verzenden", onclick: function(){
+            checkboxanswers = [];
+            checkboxes.forEach(value =>{
+               checkboxanswers.push(value.checked);
+            });
+            includedParties(answers, checkboxanswers);
+            }}, function (element){
+            container.appendChild(element);
+        });
+    }
+    else{
 
-    Object.keys(data).forEach(value =>{
-        createElement("button", {innerText: value.capitalize(), onclick: function(){
-                data[value]++;
+        createElement("h1", {innerText: questionInfo.title}, function (element) {
+            container.appendChild(element);
+        });
+        createElement("h2", {innerText: questionInfo.statement}, function (element) {
+            container.appendChild(element);
+        });
+
+        Object.keys(data).forEach(value => {
+            createElement("button", {
+                innerText: value.capitalize(), onclick: function () {
+                    answers[index] = data[value];
+                    index++;
+                    question(index, answers);
+                }
+            }, function (element) {
+                container.appendChild(element);
+            })
+        });
+        if(index > 0){
+            createElement("button", {innerText: "Vorige stelling", onclick: function(){
+                    index--;
+                    question(index, answers)
+                }}, function (element){
+                container.appendChild(element);
+            });
+        }
+        createElement("button", {innerText: "Vraag overslaan", onclick: function(){
+                answers[index] = "overgelsagen";
                 index++;
-                question(index, data);
-            }}, function(element){
-            container.appendChild(element);
-        })
-    });
-
-    console.log(JSON.stringify(data));
-
-    questionInfo.parties.forEach(value =>{
-        createElement("p", {innerText: "Naam: " + value.name}, function(element){
-            container.appendChild(element);
-            console.log(questionInfo.parties.name);
-        });
-        createElement("p", {innerText: "Mening: " + value.opinion}, function(element){
+                question(index, answers)
+            }}, function (element){
             container.appendChild(element);
         });
-    })
 
+        questionInfo.parties.forEach(value => {
+            createElement("p", {innerText: "Naam: " + value.name}, function (element) {
+                container.appendChild(element);
+            });
+            createElement("p", {innerText: "Mening: " + value.opinion}, function (element) {
+                container.appendChild(element);
+            });
+        });
+    }
+
+
+}
+
+
+
+function includedParties(answers, checkboxanswers) {
+    
 }
